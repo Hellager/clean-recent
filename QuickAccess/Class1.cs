b3a4dc9553bf731ad4beb7da012146c47a60a9f5
Hellager <86463341+Hellager@users.noreply.github.com>
2022-12-02 22:54:58 +0800

    initialize .dll project

---
+++ QuickAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickAccess
+{
+    public class Class1
+    {
+    }
+}
